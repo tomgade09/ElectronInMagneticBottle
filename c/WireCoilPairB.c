4dc42b72e3fde24bbe483c59c64eb35d69ced22f
Tom Gade <tomgade09@gmail.com>
2016-08-17 23:04:34 -0500

    Changed 'math' to 'cmath' Doh!

--- c/WireCoilPairB.c
+++ c/WireCoilPairB.c
@@ -4,19 +4,19 @@
 //using namespace std;
 
 double lfdBx(int n, double args[n]){
-    return args[1] * (-args[2]*args[5]*math::sin(args[0]) - args[2]*args[4]*math::cos(args[0]) + math::pow(args[2],2)) / math::pow((math::pow(args[3] + args[6],2) + math::pow(args[4]-args[2]*math::cos(args[0]),2) + math::pow(args[5]-args[2]*math::sin(args[0]),2)),1.5);}
+    return args[1] * (-args[2]*args[5]*cmath::sin(args[0]) - args[2]*args[4]*cmath::cos(args[0]) + cmath::pow(args[2],2)) / cmath::pow((cmath::pow(args[3] + args[6],2) + cmath::pow(args[4]-args[2]*cmath::cos(args[0]),2) + cmath::pow(args[5]-args[2]*cmath::sin(args[0]),2)),1.5);}
 
 double rtdBx(int n, double args[n]){
-    return args[1] * (-args[2]*args[5]*math::sin(args[0]) - args[2]*args[4]*math::cos(args[0]) + math::pow(args[2],2)) / math::pow((math::pow(args[3] - args[6],2) + math::pow(args[4]-args[2]*math::cos(args[0]),2) + math::pow(args[5]-args[2]*math::sin(args[0]),2)),1.5);}
+    return args[1] * (-args[2]*args[5]*cmath::sin(args[0]) - args[2]*args[4]*cmath::cos(args[0]) + cmath::pow(args[2],2)) / cmath::pow((cmath::pow(args[3] - args[6],2) + cmath::pow(args[4]-args[2]*cmath::cos(args[0]),2) + cmath::pow(args[5]-args[2]*cmath::sin(args[0]),2)),1.5);}
 
 double lfdBy(int n, double args[n]){
-    return args[1] * (args[2]*(args[3]+args[6])*math::cos(args[0])) / math::pow((math::pow(args[3] + args[6],2) + math::pow(args[4]-args[2]*math::cos(args[0]),2) + math::pow(args[5]-args[2]*math::sin(args[0]),2)),1.5);}
+    return args[1] * (args[2]*(args[3]+args[6])*cmath::cos(args[0])) / cmath::pow((cmath::pow(args[3] + args[6],2) + cmath::pow(args[4]-args[2]*cmath::cos(args[0]),2) + cmath::pow(args[5]-args[2]*cmath::sin(args[0]),2)),1.5);}
 
 double rtdBy(int n, double args[n]){
-    return args[1] * (args[2]*(args[3]-args[6])*math::cos(args[0])) / math::pow((math::pow(args[3] - args[6],2) + math::pow(args[4]-args[2]*math::cos(args[0]),2) + math::pow(args[5]-args[2]*math::sin(args[0]),2)),1.5);}
+    return args[1] * (args[2]*(args[3]-args[6])*cmath::cos(args[0])) / cmath::pow((cmath::pow(args[3] - args[6],2) + cmath::pow(args[4]-args[2]*cmath::cos(args[0]),2) + cmath::pow(args[5]-args[2]*cmath::sin(args[0]),2)),1.5);}
 
 double lfdBz(int n, double args[n]){
-    return args[1] * (args[2]*(args[3]+args[6])*math::sin(args[0])) / math::pow((math::pow(args[3] + args[6],2) + math::pow(args[4]-args[2]*math::cos(args[0]),2) + math::pow(args[5]-args[2]*math::sin(args[0]),2)),1.5);}
+    return args[1] * (args[2]*(args[3]+args[6])*cmath::sin(args[0])) / cmath::pow((cmath::pow(args[3] + args[6],2) + cmath::pow(args[4]-args[2]*cmath::cos(args[0]),2) + cmath::pow(args[5]-args[2]*cmath::sin(args[0]),2)),1.5);}
 
 double rtdBz(int n, double args[n]){
-    return args[1] * (args[2]*(args[3]-args[6])*math::sin(args[0])) / math::pow((math::pow(args[3] - args[6],2) + math::pow(args[4]-args[2]*math::cos(args[0]),2) + math::pow(args[5]-args[2]*math::sin(args[0]),2)),1.5);}
+    return args[1] * (args[2]*(args[3]-args[6])*cmath::sin(args[0])) / cmath::pow((cmath::pow(args[3] - args[6],2) + cmath::pow(args[4]-args[2]*cmath::cos(args[0]),2) + cmath::pow(args[5]-args[2]*cmath::sin(args[0]),2)),1.5);}
